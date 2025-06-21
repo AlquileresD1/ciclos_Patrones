26177a60c32c563badcb8335c9a9fd97be8e5195
AlquileresD1 <info.alquileresd1@gmail.com>
2025-06-20 18:24:01 -0600

    inicio

---
+++ Proyecto progra ll/ciclos_Patrones/src/main/java/cr/ac/ucr/ciclos_Patrones/CiclosPatronesApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.ciclos_Patrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiclosPatronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiclosPatronesApplication.class, args);
+	}
+
+}
---
+++ Proyecto progra ll/ciclos_Patrones/src/test/java/cr/ac/ucr/ciclos_Patrones/CiclosPatronesApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.ciclos_Patrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiclosPatronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
